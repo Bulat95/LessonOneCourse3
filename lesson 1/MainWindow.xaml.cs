1c5ba55b371a18713ccc7a708906d3ae1dd7a05b
t95666f <t95666f@gmail.com>
2021-04-07 03:45:14 +0400

    Первый набросок, не работает обработка кнопок

---
+++ lesson 1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace lesson_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Send1_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            textLabel.Text += str;
+        }
+        
+
+    }
+}
