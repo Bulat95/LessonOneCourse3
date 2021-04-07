db14cd51c990678111112d9ef7b878176d04f668
t95666f <t95666f@gmail.com>
2021-04-07 15:46:17 +0400

    Добавил формирование сообщения в текстовый файл

--- lesson 1/MainWindow.xaml.cs
+++ lesson 1/MainWindow.xaml.cs
@@ -1,31 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 
 
+
 namespace lesson_1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        static void ImportArrayByFile(string address, string theme, string message)
         {
-
+            string result = address + theme + message; 
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var path = Path.GetFullPath(Path.Combine(baseDir, "OUTPUT.txt"));
+            File.WriteAllText(path, result, Encoding.UTF8);
         }
 
         private void Send1_Click(object sender, RoutedEventArgs e)
         {
-            string str = (string)((Button)e.OriginalSource).Content;
-            textLabel.Text += str;
+            ImportArrayByFile(Adress.Text, ThemeMessage.Text, Message.Text);
         }
-        
-
     }
 }
